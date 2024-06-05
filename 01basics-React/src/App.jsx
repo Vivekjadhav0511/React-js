d67b587248bac23dc53a18a618cb56d88a072424
Vivek Jadhav <vivekjadhav0511@gmail.com>
2024-06-05 11:56:15 +0530

    reactApp-Vite

---
+++ 01basics-React/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Component from "./component"
+
+function App() {
+  return (
+    <> { /* fragments 
+     Naming convension 
+     1. function Name First's Character should be in UpperCase
+     2. File Name Should be with Extension Jsx if it's returnig JSX 
+     3. we Can't Return a multiple Component in React we we want to return a multiple Componentit should be Coverd With a Empty tag<></> called fragment*/} 
+     
+       <h1>React-js With Vite </h1>
+       <Component/>
+
+    </>
+  )
+}
+
+export default App
+
---
+++ 01basics-React/src/component.jsx
@@ -0,0 +1,11 @@
+
+
+function Component(){
+    return (
+        <>
+        <h1>Component Number 1</h1>
+        </>
+    )
+}
+
+export default Component
